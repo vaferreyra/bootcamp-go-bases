3736483da97ff13d1c9c44409f3ef35f56350bb5
valentinferreyraML <valentin.ferreyra@mercadolibre.com>
2022-12-26 19:24:50 -0300

    Agrego ejercicio 1 de interfaces

---
+++ dia03/ejercicio1-interfaces.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Alumno struct {
+	Nombre, Apellido string
+	DNI              int
+	FechaIngreso     string
+}
+
+func (a Alumno) String() string {
+	return fmt.Sprintf(
+		"Nombre: %s\nApellido: %s\nDNI: %v\nFechaIngreso: %s\n",
+		a.Nombre,
+		a.Apellido,
+		a.DNI,
+		a.FechaIngreso,
+	)
+}
+
+func main() {
+	var stringAlumno fmt.Stringer = Alumno{
+		"Valentin", "Ferreyra", 41924003, "19/12/2022",
+	}
+
+	fmt.Println(stringAlumno)
+}
--- dia03/main.go
+++ dia03/main.go
@@ -17,16 +17,71 @@ func (p *Persona) cumplirAños() {
 	p.Edad++
 }
 
+type Author struct {
+	FirstName, LastName string
+}
+
+func (a *Author) String() string {
+	return fmt.Sprintf("%s %s", a.FirstName, a.LastName)
+}
+
+type Book struct {
+	Title, Content string
+	Author         Author
+}
+
+func (b Book) String() string {
+	return fmt.Sprintf(
+		"El libro %s fue escrito por %s y contiene %d caracteres",
+		b.Title,
+		b.Author.String(),
+		len(b.Content),
+	)
+}
+
+func increment(value *int) {
+	*value++
+}
+
 func main() {
+	author := Author{
+		"John", "Doe",
+	}
 
-	var persona Persona
+	var textDescriptionGenerator fmt.Stringer = Book{
+		Title:   "Lord of the Rings",
+		Content: "Lorem ipsum",
+		Author:  author,
+	}
 
-	persona.Nombre = "Valentin"
-	persona.Edad = 23
-	persona.Residencia = Domicilio{Calle: "Brahms", Altura: 5732}
+	fmt.Println(textDescriptionGenerator.String())
+	// number := 10
+	// fmt.Println(number)
 
-	fmt.Println(persona.Edad)
-	persona.cumplirAños()
-	fmt.Println(persona.Edad)
+	// increment(&number)
+
+	// fmt.Println(number)
+
+	// author := Author{
+	// 	"John", "Doe",
+	// }
+
+	// book := Book{
+	// 	Title:   "Lord of the Rings",
+	// 	Content: "Lorem ipsum",
+	// 	Author:  author,
+	// }
+
+	// book.PrintInformation()
+
+	// var persona Persona
+
+	// persona.Nombre = "Valentin"
+	// persona.Edad = 23
+	// persona.Residencia = Domicilio{Calle: "Brahms", Altura: 5732}
+
+	// fmt.Println(persona.Edad)
+	// persona.cumplirAños()
+	// fmt.Println(persona.Edad)
 
 }
